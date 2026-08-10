e687793659a6ca6915a2ad160005a437712be0c5
Risto Jaskari <roikander@hotmail.com>
2026-08-10 19:36:53 +0300

    refaktoroitu kurssin osat olioiksi pääkomponentissa App (1.3: kurssitiedot step3)

--- palautusrepositorio/osa1/kurssitiedot/src/App.jsx
+++ palautusrepositorio/osa1/kurssitiedot/src/App.jsx
@@ -7,39 +7,29 @@ const Header = (props) => {
   )
 }
 
-// Content 
+// Content delegoi vanhemmaltaan App saamansa propsit ja renderöinnin Part:ille
 const Content = (props) => {
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
+      <Part name1={props.name1} exercises1={props.exercises1} />
+      <Part name2={props.name2} exercises2={props.exercises2} />
+      <Part name3={props.name3} exercises3={props.exercises3} />
     </div>
-    /*<div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exerciaes3}
-      </p>
-    </div>*/
   )
 }
 
+// Part renderöi kurssin osien nimet ja tehtävämäärän
 const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
+        {props.name1} {props.exercises1}
       </p>
       <p>
-        {props.part2} {props.exercises2}
+        {props.name2} {props.exercises2}
       </p>
       <p>
-        {props.part3} {props.exercises3}
+        {props.name3} {props.exercises3}
       </p>
     </div>
   )
@@ -54,24 +44,30 @@ const Total = (props) => {
   )
 }
 
-// App säilyttää datan ja välittää lapsikomponenteille 
-// propsien avulla tarvitsemansa datan
+// App säilyttää sovelluksen dataa ja välittää lapsikomponenteille 
+// propsien avulla niiden tarvitseman datan
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content name1={part1.name} exercises1={part1.exercises} />
+      <Content name2={part2.name} exercises2={part2.exercises} />
+      <Content name3={part3.name} exercises3={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 }
