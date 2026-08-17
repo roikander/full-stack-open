66f6d78ba8fb46223f54553c61c09cbf39b2c2f8
Risto Jaskari <roikander@hotmail.com>
2026-08-17 19:01:47 +0300

    1.6: unicafe step1 valmis

--- palautusrepositorio/osa1/unicafe/src/App.jsx
+++ palautusrepositorio/osa1/unicafe/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <h1>give feedback</h1>
+
+      <div>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
+      </div>
+
+      <h1>statistics</h1>
+
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </>
   )
 }
 
