cede3fe4c989f9ecf902bb2d82333871ea5b3b5e
Risto Jaskari <roikander@hotmail.com>
2026-08-25 12:05:16 +0300

    2.1: kurssitiedot step6 valmis

---
+++ palautusrepositorio/osa2/README.md
@@ -0,0 +1,7 @@
+# Osa 2 – Palvelimen kanssa tapahtuva kommunikointi
+
+## Tehtävät
+Allaolevissa tehtävissä luodaan React-sovelluksia Vite-työkalun avulla. Kielenä on JavaScript ja sen laajennus HTML:ää muistuttava JSX.
+
+#### kurssitiedot
+Jatkaa jo osassa 1 aloitettua sovellusta kurssitiedot, joka näyttää kurssin eri osien nimet ja tehtävämäärät.
--- palautusrepositorio/osa2/kurssitiedot/src/App.jsx
+++ palautusrepositorio/osa2/kurssitiedot/src/App.jsx
@@ -1,5 +1,35 @@
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+// Jokainen Contentin renderöimä Part-komponentti (ylhäällä)
+// renderöi yhden kurssin osan nimen ja tehtävien lukumäärän.
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+// tiivis nuolifunktio mahdollinen koska Header ei sisällä muuta kuin returnin
+const Header = ({ course }) => <h1>{course}</h1>
+
+// Course sisältää komponentit (Header ja Content), 
+// joiden vastuulle tulee kurssin nimen ja osien renderöinti
 const Course = (props) => {
-  console.log(props)
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
 }
 
 const App = () => {
