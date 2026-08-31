629aebaf4741a77232bd454b009297e39d0d72f9
Risto Jaskari <roikander@hotmail.com>
2026-08-31 10:53:57 +0300

    2.8: puhelinluettelo step3 valmis (henkilöt <p> --> <div>)
    q

--- palautusrepositorio/osa2/puhelinluettelo/src/App.jsx
+++ palautusrepositorio/osa2/puhelinluettelo/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
       <h2>Numbers</h2>
 
       {persons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
+        <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
   )
