41c2443e609f8e29ccb2402238b818fea3a0baec
Risto Jaskari <roikander@hotmail.com>
2026-09-09 13:02:37 +0300

    2.16: puhelinluettelo step11 valmis

--- palautusrepositorio/osa2/puhelinluettelo/src/components/Notification.jsx
+++ palautusrepositorio/osa2/puhelinluettelo/src/components/Notification.jsx
@@ -3,11 +3,21 @@ const Notification = ({ message }) => {
     return null
   }
 
-  return (
-    <div className="notification">
-      {message}
-    </div>
-  )
+  const notificationStyle = {
+    color: 'rgb(155, 57, 155)',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+}
+
+return (
+  <div style={notificationStyle}>
+    {message}
+  </div>
+)
 }
 
 export default Notification
