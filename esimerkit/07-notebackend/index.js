8811be69188126f797585184cf6764382cc761e5
Risto Jaskari <roikander@hotmail.com>
2026-09-15 18:26:27 +0300

    esimerkkiin pari DIY-middlewarea

--- esimerkit/07-notebackend/index.js
+++ esimerkit/07-notebackend/index.js
@@ -1,96 +1,116 @@
-// Otetaan käyttöön express, joka on tällä kertaa funktio, jota kutsumalla 
+// Otetaan käyttöön express, joka on tällä kertaa funktio, jota kutsumalla
 // luodaan muuttujaan app sijoitettava Express-sovellusta vastaava olio.
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let notes = [
   {
     id: "1",
     content: "HTML is easy",
-    important: true
+    important: true,
   },
   {
     id: "2",
     content: "Browser can execute only JavaScript",
-    important: false
+    important: false,
   },
   {
     id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+    important: true,
+  },
+];
+
+// Itse määritelty middleware, joka tulostaa npm-konsoliin 
+// palvelimelle tulevien pyyntöjen perustietoja, 
+// lopussa oleva next() siirtää kontrollin seuraavalle middlewarelle.
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
 // Expressin json-parser käyttöön -> lähettettyyn dataan pääsee helposti käsiksi
-app.use(express.json())
+app.use(express.json());
+app.use(requestLogger);
 
-// Tapahtumankäsittelijäfunktiolla on kaksi parametria. Näistä ensimmäinen eli 
-// request sisältää kaikki HTTP-pyynnön tiedot ja toisen parametrin response:n 
+// Tapahtumankäsittelijäfunktiolla on kaksi parametria. Näistä ensimmäinen eli
+// request sisältää kaikki HTTP-pyynnön tiedot ja toisen parametrin response:n
 // avulla määritellään, miten pyyntöön vastataan.
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
 // Polkuun /api/notes tulleisiin pyyntöihin vastataan muistiinpanot taulukolla,
 // joka muutetaan response-olion json-metodilla JSON-muotoiseksi merkkijonoksi.
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
 
-// Yksittäisen resurssin voi hakea antamalla polkuun/URLiin kaksoispisteen 
-// jälkeen haettavasta kohteesta löytyvä parametri (tässä id), 
+// Yksittäisen resurssin voi hakea antamalla polkuun/URLiin kaksoispisteen
+// jälkeen haettavasta kohteesta löytyvä parametri (tässä id),
 // käsiksi siihen päästään request-olion avulla.
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
 
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
-  }  
-})
+    response.status(404).end();
+  }
+});
 
 // Poisto tapahtuu tekemällä HTTP DELETE ‑pyyntö resurssin urliin.
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-// Luo uudelle muistiinpanolle uniikin id:n, joka on 
+// Luo uudelle muistiinpanolle uniikin id:n, joka on
 // yhdellä suurempi kuin tällä hetkellä suurin id.
 const generateId = () => {
   const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+    ? Math.max(...notes.map((n) => Number(n.id)))
+    : 0;
+  return String(maxId + 1);
+};
 
 // uusi muistiinpano lisätään POST-pyynnöllä, jos kenttä content puuttuu -> 400,
 // jos kenttä important puuttuu -> aseta false siihen
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-const PORT = 3001
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// Middleware jonka ansiosta saadaan polkujen käsittelemättömistä 
+// virhetilanteista JSON-muotoinen virheilmoitus.
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- palautusrepositorio/osa3/puhelinluettelon-backend/index.js
+++ palautusrepositorio/osa3/puhelinluettelon-backend/index.js
@@ -1,93 +1,91 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
   {
     id: "1",
     name: "Arto Hellas",
-    number: "040-123456"
+    number: "040-123456",
   },
   {
     id: "2",
     name: "Ada Lovelace",
-    number: "39-44-5323523"
+    number: "39-44-5323523",
   },
   {
     id: "3",
     name: "Dan Abramov",
-    number: "12-43-2345"
+    number: "12-43-2345",
   },
   {
     id: "4",
     name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
+    number: "39-23-6423122",
+  },
+];
 
 const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max)
-}
+  return Math.floor(Math.random() * max);
+};
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
 
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.get('/info', (request, response) => {
-  response.send
-  (`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`)
-})
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`);
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-  
-  response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  console.log(body)
+  response.status(204).end();
+});
 
-if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing'    
-    })  
-} else if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique'    
-    })  
-}
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  } else if (persons.some((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
   const person = {
     id: JSON.stringify(getRandomInt(100000)),
     name: body.name,
-    number: body.number
-  }
+    number: body.number,
+  };
 
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = 3001
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on portti ${PORT}`)
-})
+const PORT = 3001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on portti ${PORT}`);
+});
