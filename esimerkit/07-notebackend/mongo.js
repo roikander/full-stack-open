cf4acd93b6e238e5abcd198a2503ded79598310e
Risto Jaskari <roikander@hotmail.com>
2026-09-23 13:28:03 +0300

    mongodb-harjoittelua

---
+++ esimerkit/07-notebackend/mongo.js
@@ -0,0 +1,49 @@
+// erillinen kokeilusovellus/tiedosto mongodb:tä ja mongoosea varten
+const mongoose = require('mongoose')
+
+// process.argv = ohjelman käynnistyksessä annetut komentoriviargumentit.
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+// Ottaa komentoriviargumenttien kolmannen arvon, asettaa sen salasanaksi
+// muuttujaan password ja käyttää sitä kirjautumiseen muuttujassa url. 
+const password = process.argv[2]
+const url = `mongodb+srv://roikander_db_user:${password}@cluster0.tiadrfo.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+// muodosta yhteys tietokantaan, parametrina tietokannan url ja yhteystapa ipv4 
+mongoose.connect(url, { family: 4 })
+
+// määrittelee minkälaisia dokumentteja tämä notes-kokoelma sisältää  
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// eka parametri 'Note' kertoo että talleta oliot notes-kokoelmaan
+const Note = mongoose.model('Note', noteSchema)
+
+// Hakee tärkeät muistiinpanot, jos find-metodin parametri olisi 
+// tyhjä taulukko {}, palautettaisiin kaikki muistiinpanot.
+Note.find({ important: true }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// luo uuden note-olion skeeman mukaisesti, model:in avulla
+/*const note = new Note({
+  content: 'One More Time',
+  important: false,
+})
+
+// Tallettaa metodilla save uuden muistiinpanon ja 
+// lopuksi sammuttaa tietokantayhteyden.
+note.save().then(result => {
+  console.log('note saved!')
+  console.log(result.important)     // true tai false
+  mongoose.connection.close()
+})*/
